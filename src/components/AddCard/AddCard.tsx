47238af023789290d3c99d9152208ac33078537c
Corey Keller <corey.james.keller@gmail.com>
2019-04-03 17:41:04 -0600

    adding fields to add card form

--- src/components/AddCard/AddCard.tsx
+++ src/components/AddCard/AddCard.tsx
@@ -6,34 +6,49 @@ import { IAddCardProps, IAddCardState } from './IAddCard';
 import { withAuthorization, AuthUserContext } from '../Session';
 import { withRouter } from 'react-router-dom';
 
-class AddCardBase extends Component<IAddCardProps, any, IAddCardState> {
+class AddCardBase extends Component<IAddCardProps, IAddCardState> {
     constructor(props: IAddCardProps) {
         super(props);
 
         this.state = {
+            error: '',
             name: '',
             type: '',
-            error: '',
+            cardType: '',
+            opened: '',
+            creditLimit: 0,
+            annualFee: 0,
+            firstYearFree: false,
+            rewardName: '',
+            rewardAmount: 0,
+            minimumSpend: 0,
+            timeFrame: 0,
+            estimatedValue: 0,
         };
     }
 
     addNewCard() {
-        const uid = this.props.firebase.auth.currentUser.uid;
+        if (this.props.firebase.auth.currentUser) {
+            const uid = this.props.firebase.auth.currentUser.uid;
 
-        this.props.firebase
-            .cards(uid)
-            .push({
-                name: this.state.name,
-                type: this.state.type,
-            })
-            .then(() => {
-                this.setState({ ...this.state });
-                this.props.history.push(ROUTES.HOME);
-            })
-            .catch((error: any) => {
-                console.log(error);
-                this.setState({ error });
-            });
+            this.props.firebase
+                .cards(uid)
+                .push({
+                    name: this.state.name,
+                    type: this.state.type,
+                    cardType: this.state.cardType,
+                    opened: this.state.opened,
+                    creditLimit: this.state.creditLimit,
+                })
+                .then(() => {
+                    this.setState({ ...this.state });
+                    this.props.history.push(ROUTES.HOME);
+                })
+                .catch((error: any) => {
+                    console.log(error);
+                    this.setState({ error });
+                });
+        }
     }
 
     onChange = (e: any) => {
@@ -69,14 +84,31 @@ class AddCardBase extends Component<IAddCardProps, any, IAddCardState> {
                                         type="text"
                                     />
                                     <label>Type</label>
-                                    <input
-                                        name="type"
-                                        onChange={this.onChange}
-                                        value={this.state.type}
-                                        type="text"
-                                    />
+                                    <select name="type" defaultValue="" onChange={this.onChange}>
+                                        <option value="" disabled>select</option>
+                                        <option value="personal">Personal</option>
+                                        <option value="business">Business</option>
+                                    </select>
+                                    <label>Card Type</label>
+                                    <select name="cardType" defaultValue="" onChange={this.onChange}>
+                                        <option value="" disabled>select</option>
+                                        <option value="Mastercard">Mastercard</option>
+                                        <option value="Visa">Visa</option>
+                                        <option value="Amex">Amex</option>
+                                    </select>
                                     <label>Opened</label>
-                                    <input type="date" />
+                                    <input
+                                        name="opended"
+                                        onChange={this.onChange}
+                                        type="date"
+                                        value={this.state.opened} />
+                                    <label>Credit Limit</label>
+                                    <input
+                                        name="creditLimit"
+                                        onChange={this.onChange}
+                                        type="number"
+                                        value={this.state.creditLimit}
+                                        step="any" />
                                     <button type="submit">Add</button>
                                 </form>
                             ) : null
--- src/components/AddCard/IAddCard.ts
+++ src/components/AddCard/IAddCard.ts
@@ -1,12 +1,25 @@
 import { RouteComponentProps } from 'react-router-dom';
+import Firebase from '../Firebase';
 
 export interface IAddCardProps extends RouteComponentProps<any> {
     toggleNav: any;
-    firebase: any;
+    firebase: Firebase;
 }
 
 export interface IAddCardState {
     error: string;
     name: string;
     type: string;
+    cardType: string;
+    opened: string | number | string[] | undefined;
+    creditLimit: number;
+    annualFee: number;
+    firstYearFree: boolean;
+    rewardName: string;
+    rewardAmount: number;
+    minimumSpend: number;
+    timeFrame: number;
+    estimatedValue: number;
+    [x: number]: any;
+
 }
